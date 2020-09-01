da509d9ad4a69e758ba3774e1a157197d6e30cf1
EliotChang <eliotcha@usc.edu>
2020-08-31 18:22:39 -0700

    initial commit

--- scout/src/App.js
+++ scout/src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import image from './image.png';
+import table from './table'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "app">
+      <div className = "hero">
+        <div className = "top">
+          <div className = "image">
+            <img src = {image}/>
+          </div>
+          <div className = "introText">
+            <p> Just a crowdsourced list of internships. </p>
+            <p> Scout. </p>
+          </div>
+        </div>
+
+        <div className = "input">
+          <form>
+            <label>
+              Name:
+              <input type="text" name="name" />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      </div>
+
+      <div className = "updates">
+        <p> 42 positions open </p>
+        <p> Last updated: 8/27/2020 </p>
+      </div>
+
+      <div className = "listView">
+        <table></table>
+      </div>
+
+      <div className = "footer">
+        <p> maintained by @eliotchang & @andrwyang. </p>
+      </div>
     </div>
   );
 }
