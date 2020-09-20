ddc92a2fdf150cb0a13e0986fbacafd80ead8778
EliotChang <eliotcha@usc.edu>
2020-09-20 12:35:59 -0700

    react-ify step 3: formatting

--- scout/src/components/PhoneInput.js
+++ scout/src/components/PhoneInput.js
@@ -3,8 +3,17 @@ function PhoneInput(){
     return (
         <div className='PhoneInput'>
             <form>
-                <input className= "input" type="text" name="name" placeholder="New job who dis? Drop your phone number to stay updated." />
-                <input className= "submit" type="submit" value="Sign Up" />
+                <input
+                    className="input"
+                    type="text"
+                    name="name"
+                    placeholder="New job who dis? Drop your phone number to stay updated."
+                />
+                <input
+                    className="submit"
+                    type="submit"
+                    value="Sign Up"
+                />
             </form>
         </div>
     );
